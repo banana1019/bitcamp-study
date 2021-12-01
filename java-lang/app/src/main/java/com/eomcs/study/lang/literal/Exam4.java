ed907fcc8a1c67762a9c3393cfd22a13e2dbbf3b
banana1019 <jyho930@gmail.com>
2021-12-01 09:47:23 +0900

    2021-12-01

--- java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam4.java
+++ java-lang/app/src/main/java/com/eomcs/study/lang/literal/Exam4.java
@@ -12,13 +12,25 @@ public class Exam4 {
 
   @GetMapping("/test1")
   public String test1() {
-    float value = 987.654321f; // 4바이트 메모리 사용. 유효자릿수를 넘어가는 값은 잘린다. 
+    float value = 987.6543f; // 4바이트 메모리 사용. 유효자릿수를 넘어가는 값은 잘린다. 
     return "부동소수점: " + value;
   }
 
   @GetMapping("/test2")
   public String test2() {
-    double value = 987.654321; // 8바이트 메모리 사용
+    double value = 987654321.1234567; // 8바이트 메모리 사용
+    return "부동소수점: " + value;
+  }
+
+  @GetMapping("/test3")
+  public String test3() {
+    float value = 987.654321f; // 4바이트 메모리 크기(유효자릿수 7자리)를 넘어서는 값은 잘린다. 유효자릿수를 넘어가는 값은 잘린다. 
+    return "부동소수점: " + value;
+  }
+
+  @GetMapping("/test4")
+  public String test4() {
+    double value = 987654321.12345678987654; // 8바이트 메모리 크기(유효자릿수 15자리)를 넘어서는 값은 잘린다.
     return "부동소수점: " + value;
   }
 
