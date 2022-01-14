85bcdf5d499b998de711bd385277a8bd94ccc0d7
banana1019 <jyho930@gmail.com>
2022-01-14 12:40:00 +0900

    실전 프로그래밍 3단계 - 데코레이터 설계 기법 적용

--- java-lang/app/src/main/java/com/eomcs/oop/ex05/x6/Truck.java
+++ java-lang/app/src/main/java/com/eomcs/oop/ex05/x6/Truck.java
@@ -5,6 +5,29 @@ public class Truck extends Car {
 
   @Override
   public void run() {
+    // 기존에 이미 있는 메서드를 호출한다.
+    this.go();
+  }
+
+  @Override
+  public void start() {
+    this.launch();
+  }
+
+  @Override
+  public void stop() {
+    this.stopping();
+  }
+
+  public void launch() {
+    System.out.println("트럭 시동 건다!");
+  }
+
+  public void stopping() {
+    System.out.println("트럭 시동 끈다!");
+  }
+
+  public void go() {
     System.out.println("덜컹덜컹 달린다!");
   }
 
